6c3fb07238b93ee0588536550380d93d28401eb1
Ansh-mishra21 <mishransh2108@gmail.com>
2026-03-11 02:05:03 +0530

    Refactor CORS middleware configuration in server.js for improved security and flexibility

--- server/server.js
+++ server/server.js
@@ -14,7 +14,11 @@ const reviewRoutes = require("./routes/reviewRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
