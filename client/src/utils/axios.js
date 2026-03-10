dd7a718a4c899a48c37b7dd3b8eeb784f34ec924
Ansh-mishra21 <mishransh2108@gmail.com>
2026-03-11 00:12:37 +0530

    Fix axios instance base URL to use import.meta.env for environment variable

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with backend base URL
 const api = axios.create({
-  baseURL: `${process.env.VITE_SERVER_URL}/api`,
+  baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
 });
 
 // Interceptor to automatically attach JWT token to every request
